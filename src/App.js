4810f76690e18444991fd148abfdaa2f5d0e49bb
Priyansu Dash <priyansud796@gmail.com>
2024-09-18 21:12:21 +0530

    adding data which is constamt.js and design the searchbar and navbar for now to show up on screen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => (
   <BrowserRouter>
     <Box sx={{backgroundColor: '#000'}}>
       <Navbar/>
-      <Searchbar/>
       <Routes>
         <Route path='/' exact element={<Feed/>}/>
         <Route path='/video/:id' exact element = {<VideoDetails/>}/>
